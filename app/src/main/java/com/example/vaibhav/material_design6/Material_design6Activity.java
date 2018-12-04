ca8833509e41983f303e6269e254d3201c90a483
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-04 16:37:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/material_design6/Material_design6Activity.java
@@ -0,0 +1,68 @@
+package com.example.vaibhav.material_design6;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Material_design6Activity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tv1, tv2, tv3, tv4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_design6);
+
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
+
+        tv1.setOnClickListener(this);
+        tv2.setOnClickListener(this);
+        tv3.setOnClickListener(this);
+        tv4.setOnClickListener(this);
+
+        tv1.setTextColor(Color.parseColor("#FF69B4"));
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.tv1:
+                tv1.setTextColor(Color.parseColor("#FF69B4"));
+                tv2.setTextColor(Color.parseColor("#000000"));
+                tv3.setTextColor(Color.parseColor("#000000"));
+                tv4.setTextColor(Color.parseColor("#000000"));
+                break;
+
+            case R.id.tv2:
+                tv1.setTextColor(Color.parseColor("#000000"));
+                tv2.setTextColor(Color.parseColor("#FF69B4"));
+                tv3.setTextColor(Color.parseColor("#000000"));
+                tv4.setTextColor(Color.parseColor("#000000"));
+                break;
+
+            case R.id.tv3:
+                tv1.setTextColor(Color.parseColor("#000000"));
+                tv2.setTextColor(Color.parseColor("#000000"));
+                tv3.setTextColor(Color.parseColor("#FF69B4"));
+                tv4.setTextColor(Color.parseColor("#000000"));
+                break;
+
+            case R.id.tv4:
+                tv1.setTextColor(Color.parseColor("#000000"));
+                tv2.setTextColor(Color.parseColor("#000000"));
+                tv3.setTextColor(Color.parseColor("#000000"));
+                tv4.setTextColor(Color.parseColor("#FF69B4"));
+                break;
+
+                }
+
+        }
+
+}
